f514a2887003e31745793d786c571b586c117eec
milosdjurica <milosdjurica13@gmail.com>
2024-02-04 21:23:14 +0100

    dark mode button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { DarkModeButton } from "@/components/DarkModeButton";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex ">
       <h1 className="text-red-700 text-8xl">Hello</h1>
+      <DarkModeButton />
     </main>
   );
 }
